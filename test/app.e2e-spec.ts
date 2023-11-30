d7dff4a55d05dc5c6c4ad62815cadba2b7e7ef8e
Diego Palacios <palaciosdiegovalentino@gmail.com>
2023-11-30 08:21:34 -0300

    Add: e2e tests for singup & signin

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';  // Adjust the path based on your project structure
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from '../src/prisma/prisma.service';
+import * as pactum from 'pactum';
+import { AuthDto } from '../src/auth/dto'
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -14,10 +18,44 @@ describe('App e2e', () => {
         whitelist: true,
       })
     );
+
     await app.init();
+    await app.listen(3333)
+    prisma = app.get(PrismaService)
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333')
   });
+
   afterAll(()=>{
     app.close();
   })
-  it.todo('should pass');
+  describe('Auth', ()=> {
+    const dto: AuthDto = {
+      email: 'palaciosdiegovalentino@gmail.com',
+      password: '123',
+    };
+    describe('Signup', ()=> {
+      it('should sign up', ()=>{
+        return pactum.spec().post(
+          '/auth/signup',
+        ).withBody(dto).expectStatus(201).inspect()
+      })
+    });
+
+
+    describe('Signin', ()=> {
+      it('should sign in', ()=>{
+        return pactum.spec().post(
+          '/auth/signin',
+        ).withBody(dto).expectStatus(200).inspect()
+      })
+    });
+  })
+
+  describe('User', ()=>{
+    describe('Get me', ()=>{})
+    describe('Edit user', ()=>{})
+  })
+
+  describe('Bookmarks', ()=>{})
 });
