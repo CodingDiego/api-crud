14bf5f19f598a31d3d30d50410b9f6a404b68213
Diego Palacios <palaciosdiegovalentino@gmail.com>
2023-11-29 21:58:57 -0300

    NEW:Auth logic and db url in .env

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,28 +1,46 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
-import { AuthDto } from "./dto";
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
-export class AuthService{
-    constructor(private prisma: PrismaService){}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
 
-    async signup(dto: AuthDto){
-        const hash = await argon.hash(dto.password)
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash
-            }
-        })
-
-
-        return user;
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials Taken');
+        }
+      }
+      throw error;
     }
+  }
 
-    singin(){
-        return {msg: 'I am signing in'}
-    }
+  async singin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw new ForbiddenException('Credentials Incorrect');
 
-    
-};
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    if (!pwMatches) throw new ForbiddenException('Credentials Incorrect');
+
+    delete user.hash
+    return user;
+  }
+}
